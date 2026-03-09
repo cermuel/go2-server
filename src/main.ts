eb85fb7a56e451725ce453eb7f2b3ab987a9125c
Ngene Samuel Obasi <81524684+cermuel@users.noreply.github.com>
2026-03-09 23:50:02 +0100

    fix: cors error on frontend

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:5173'],
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:5173",
+      "https://go22.vercel.app",
+    ],
   });
   app.useGlobalPipes(new ValidationPipe());
 
